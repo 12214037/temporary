07507b3e8cfa1351be39871d0c98d30a3980747d
12214037 <pmb087@naer.com>
2021-09-25 16:49:40 +0900

    v0.5 prime number

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,22 +1,46 @@
 import java. util.Scanner;
-// V0.3
+// V0.5
 public class ReviewDemo01 {
    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         boolean isPrime = true;
 
-       for(int k=2; k<number; k++){
-            if(number % k == 0)
-               isPrime = false;
-        }
-
-        if(isPrime)
-            System.out.println(number + "은(는) 소수!");
-        else
-          System.out.println(number + "은(는) 소수가 아닙니다");
-    }
+       int k=2;
+       while(k<number){
+           if(number % k == 0) {
+              isPrime = false;
+              break;
+         }
+           k++;
+       }
+           if(isPrime)
+              System.out.println(number + "은(는) 소수!");
+           else
+              System.out.println(number + "은(는) 소수가 아닙니다");
+   }
 }
+
+
+//import java. util.Scanner;
+//// V0.3
+//public class ReviewDemo01 {
+//   public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        boolean isPrime = true;
+//
+//       for(int k=2; k<number; k++){
+//            if(number % k == 0)
+//               isPrime = false;
+//        }
+//
+//        if(isPrime)
+//            System.out.println(number + "은(는) 소수!");
+//        else
+//          System.out.println(number + "은(는) 소수가 아닙니다");
+//    }
+//}
 
 
 //import java. util.Scanner;
